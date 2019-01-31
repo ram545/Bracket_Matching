950dbbf4be45773149f31fea8b0c6819989280a3
Rama Krishna Reddy Sabbella <20751687+ram545@users.noreply.github.com>
2019-01-31 23:03:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "./stack.h"
 
 int main(){
-    int maxDepth=0,depth=0,i,len,line=0;
+    int maxDepth=0,depth=0,i,len,line=0,pair=0;
+    bool emptycheck;
     Stack<char> s;
     string str;
-    ofstream fd;
-    fd = open("stack.h");
-    while(getline(fd,str)!=EOF){
-        if(str.length()>0)
-            len++;
+    fstream fd("test.txt",ios::in | ios::out);
+    while(fd){
+        getline(fd,str);
+        line++;
         len=str.length();
         for(i=0;i<len;i++){
             if(str[i]=='{' || str[i]=='[' || str[i]=='('){
@@ -17,24 +17,28 @@ int main(){
                 if(depth>maxDepth)
                     maxDepth=depth;
             }
-            if(str[i]=='}' || str[i]==']' || str[i]==')'){
+            else if(str[i]=='}' || str[i]==']' || str[i]==')'){
                 if(s.peek()=='{' && str[i]=='}'){
                     s.pop();
                     depth--;
+                    pair++;
                 }
                 else if(s.peek()=='[' && str[i]==']'){
                     s.pop();
                     depth--;
+                    pair++;
                 }
                 else if(s.peek()=='(' && str[i]==')'){
                     s.pop();
                     depth--;
+                    pair++;
                 }
                 else{
-                    cout << "Bracket Missing in line number: " << line << endl;
+                    cout << "Bracket Missing in line number " << line << " at index: " << i << str[i] << endl;
                 }
             }
         }
     }
+    cout << "Depth: " << maxDepth << "  " << "No of pairs: " << pair << endl;
     return 0;
 }
