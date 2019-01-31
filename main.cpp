c1134179b38b2980e30b1f34eed36eb65cc1ac1d
Rama Krishna Reddy Sabbella <20751687+ram545@users.noreply.github.com>
2019-01-31 18:37:27 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include "./stack.h"
 
 int main(){
-    int maxDepth=0,depth=0,i,len;
-    Stack s;
-    fstream fd;
+    int maxDepth=0,depth=0,i,len,line=0;
+    Stack<char> s;
+    string str;
+    ofstream fd;
     fd = open("stack.h");
-
+    while(getline(fd,str)!=EOF){
+        if(str.length()>0)
+            len++;
+        len=str.length();
+        for(i=0;i<len;i++){
+            if(str[i]=='{' || str[i]=='[' || str[i]=='('){
+                s.push(str[i]);
+                depth++;
+                if(depth>maxDepth)
+                    maxDepth=depth;
+            }
+            if(str[i]=='}' || str[i]==']' || str[i]==')'){
+                if(s.peek()=='{' && str[i]=='}'){
+                    s.pop();
+                    depth--;
+                }
+                else if(s.peek()=='[' && str[i]==']'){
+                    s.pop();
+                    depth--;
+                }
+                else if(s.peek()=='(' && str[i]==')'){
+                    s.pop();
+                    depth--;
+                }
+                else{
+                    cout << "Bracket Missing in line number: " << line << endl;
+                }
+            }
+        }
+    }
     return 0;
 }
